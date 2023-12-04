fae1bbcc593c81bfc7c7367b61c13ccfbf719aed
Smalls141414 <ari.grady14@gmail.com>
2023-12-04 15:19:22 -0800

    Resources Page Completed

--- frontend/src/components/Resources.js
+++ frontend/src/components/Resources.js
@@ -1,22 +1,48 @@
 import React from 'react';
 
 function Resources() {
+		
+// 	const styles = StyleSheet.create({
+// 		container: {
+// 			width: 200
+// 		}
+// 	})
+
 	return(
 		<section>
 			<div background={"white"}>
 				<h1 class="mt-5" > <u>Campus Resources</u></h1>
-				<h2 class="mt-5">Campus Resources</h2>
-				<p>Spartan Bulletin Home - woohoo!</p>
-				<h2 class="mt-5">Campus Resources 2</h2>
-				<p>Spartan Bulletin Home - woohoo!</p>
-				<h2 class="mt-5">Campus Resources 3</h2>
-				<p>Spartan Bulletin Home - woohoo!</p>
-				<h2 class="mt-5">Campus Resources 4</h2>
-				<p>Spartan Bulletin Home - woohoo!</p>
-				<h2 class="mt-5">Campus Resources 5</h2>
-				<p>Spartan Bulletin Home - woohoo!</p>
-				<h2 class="mt-5">Campus Resources 6</h2>
-				<p>Spartan Bulletin Home - woohoo!</p>
+				<h2 class="mt-5"><a href="https://www.sjsu.edu/aec/index.php?">Accessible Education Center</a></h2>
+				<p>"The Accessible Education Center, through collaboration with the campus and community, is committed to redefining</p>
+				<p>ability for students with disabilities by providing comprehensive services in support of the educational development</p>
+				<p>and success of students with disabilities in a student-centered and professional environment"</p>
+				
+				<h2 class="mt-5"><a href="https://www.sjsu.edu/careercenter/index.php">Career Center</a></h2>
+				<p>"The Career Center enables students to explore majors and careers, gain relevant experience and professional confidence,</p>
+				<p>search for meaningful work and connect with exciting employers. All students are eligible to receive services from the </p>
+				<p>Career Center from resume writing to interviewing skills. The Career Center’s vision is that every Spartan emerges career </p>
+				<p>ready and equipped for lifelong career success."</p>
+				
+				<h2 class="mt-5"><a href="https://www.sjsu.edu/peerconnections/index.php">Peer Connections</a></h2>
+				<p>"Peer Connections supports students through tutoring, mentoring, supplemental instructions, learning assistants and student </p>
+				<p>success events in an inclusive, welcoming environment. It serves as a university-wide resource for the promotion of peer</p>				
+				<p>learning and retention."</p>
+				
+				<h2 class="mt-5"><a href="https://www.sjsu.edu/faso/">Financial Aid and Scholarship Office</a></h2>
+				<p>"The Financial Aid and Scholarship Office provides quality educational financing options, while delivering consistent, equitable,</p>
+				<p>and timely services to student aid applicants at San José State University. In addition to federal and state funding, the Financial</p>
+				<p>Aid and Scholarship Office connects students with campus scholarship opportunities and other off campus resources."</p>
+				
+				<h2 class="mt-5"><a href="https://www.sjsu.edu/studenthealth/">Student Health Center</a></h2>
+				<p>"The SJSU Student Health Center serves all registered students by providing treatment and care in support of the student community’s</p>
+				<p>well-being. You can receive attention for acute conditions, injuries, and illnesses from licensed physicians, and support services</p>
+				<p>such as a laboratory, pharmacy, physical therapy, radiology, dermatology, podiatry and sports medicine. The Wellness and Health </p>
+				<p>Promotion team provides support in educating the student community on a variety of topics such as stress, sleep, healthy relationships, </p>
+				<p>nutrition, alcohol use, sexual wellness, and much more."</p>
+				
+				<h2 class="mt-5"><a href="https://www.sjsu.edu/learnanywhere/campus-resources/index.php">Further Resources</a></h2>
+				<p>More in-depth resources for SJSU students can be found on the SJSU Campus Resources website linked here!</p>
+				
 			</div>
 		</section>
 	);
