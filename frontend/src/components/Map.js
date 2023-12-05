3efd6648b7b0a5ceaa47f656ff9ca21138a57b21
Smalls141414 <ari.grady14@gmail.com>
2023-12-04 22:55:51 -0800

    Added multiple pins with text

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,35 +1,70 @@
 import React from 'react';
+import { useState } from 'react';
 import {APIProvider, Map, AdvancedMarker, Pin, InfoWindow} from '@vis.gl/react-google-maps';
 
+//TODO:
+// - Allow click to get lat/long
+// - Connect Database to Map Pointers
+
+
+
 function SpartanMap() {
-	const position = { lat:37.33526 , lng:-121.88149 };
-	
+	// MAP BOUNDS AND CENTER
+	const mCenter = {lat:37.33526 , lng:-121.88149};
 	const SjsuBounds = {
-	north: 37.34010,
-	south: 37.33082,
-	east: -121.87521,
-	west: -121.88781,
+	latLngBounds: {north: 37.35827484264925,
+	south: 37.30679211245778,
+	east: -121.84188065174233,
+	west: -121.93344724310798,},
+	
+	strictBounds: false
 	};
 	
-	//Map Bounds not working
 	
+	//Test positions
+	var positions = [];
+	positions.push({id: 0, mapp:{lat:37.33526 , lng:-121.88189}, club:"John Club", text:"HandDoorHandHookCarDoor"});
+	positions.push({id: 1, mapp:{lat:37.33528 , lng:-121.88148}, club:"Non-John Club", text:"Poems"});
+	
+	
+	//SET STATE
+	const [open,setOpen] = useState(false);
+	
+	//Individual Marker HTML
+	const posList = positions.map((p) =>
+		<div>
+        	<AdvancedMarker position={p.mapp} onClick={()=>setOpen(true)}>
+				<Pin
+				background={"blue"}
+				borderColor={"black"}
+				glyphColor={"yellow"}>
+				</Pin>
+			</AdvancedMarker>
+			
+			{open && <InfoWindow position={p.mapp} onCloseClick={()=>setOpen(false)}>
+			<p><b>{p.club}</b></p>
+			<p>{p.text}</p>
+			</InfoWindow>}
+			
+		</div>
+    );
+	
+	//RETURN HTML
 	return(
 		<section>
 			<div style = {{height: "93vh"}}>
-				<APIProvider apiKey={'INSERT API KEY HERE'}>
+				<APIProvider apiKey={'API KEY HERE'}>
 					<Map zoom={17} 
-					center={position} 
+					center={mCenter} 
 					mapId={'de8660dc41ef3e4f'}
 					heading={329.3}
 					restriction={SjsuBounds}
+					onClick={ev => {
+      				console.log("latitide = ", ev.latLng());
+      				console.log("longitude = ", ev.latLng());
+    				}}
 					>
-						<AdvancedMarker position={position}>
-							<Pin
-							background={"blue"}
-							borderColor={"black"}
-							glyphColor={"yellow"}>
-							</Pin>
-						</AdvancedMarker>
+						{posList}
 					</Map>
 				</APIProvider>
 			</div>
